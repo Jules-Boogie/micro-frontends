7f9da69b92fdfb1a93fd39b842044ffb9b0185eb
Juliet George <georgejuliet11@gmail.com>
2021-07-27 16:03:07 -0700

    fix css className for production styling

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -2,16 +2,20 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName} from '@material-ui/styles';
 
 export default () => {
+    const generateClassName = createGenerateClassName({
+        productionPrefix:'co'
+    })
+    
   return(
 <BrowserRouter>
-    <>
+    <StylesProvider generateClassName={generateClassName}>
     <Header />
       <MarketingApp />
-    </>
+    </StylesProvider>
   </BrowserRouter>
   )
-
   
 };
