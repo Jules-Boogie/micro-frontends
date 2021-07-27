61079b8029eae867ff3db0fa5023ec68ac5e684b
Juliet George <georgejuliet11@gmail.com>
2021-07-26 17:06:55 -0700

    created container deploy script

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import {mount} from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp'
 
-console.log(mount)
 export default ()=> {
     return <>
     <h1> hi from container </h1>
